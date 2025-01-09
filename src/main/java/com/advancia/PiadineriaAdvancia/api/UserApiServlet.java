f78e32331477cb9f8b89bb651b45a45a1e75bcb8
Loris <farrauto.loris@gmail.com>
2025-01-09 11:16:35 +0100

    Modificata la loginServlet per utilizzare l'api REST.

--- src/main/java/com/advancia/PiadineriaAdvancia/api/UserApiServlet.java
+++ src/main/java/com/advancia/PiadineriaAdvancia/api/UserApiServlet.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,6 +18,7 @@ public class UserApiServlet {
     
     @POST
     @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response login(LoginRequest request) {
         Employee employee = userService.getUser(request.getUsername(), request.getPassword());
